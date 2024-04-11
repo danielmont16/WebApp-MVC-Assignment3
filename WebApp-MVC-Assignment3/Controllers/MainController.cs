88fd7695f2b3cb8ef54f674e96efcd308e14e53f
Daniel Montenegro <danielmontenegro@daniels-air.home>
2024-04-11 19:24:48 -0400

    first Commit

---
+++ WebApp-MVC-Assignment3/Controllers/MainController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//custome controller created manually
+namespace WebApp_MVC_Assignment3.Controllers
+{
+    public class MainController : Controller
+    {
+       //adding the methods for each page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutPage()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+    }
+}
+
